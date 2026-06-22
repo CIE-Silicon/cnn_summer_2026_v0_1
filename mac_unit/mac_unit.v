631579b16fb4d2e1f8484ebd0ca188f13c942720
Shashank-T1wari <shankstiwary@gmail.com>
2026-06-23 01:00:04 +0530

    modified mac unit with option to use variable number of macs as per parameter

--- mac_unit/mac_unit.v
+++ mac_unit/mac_unit.v
@@ -1,34 +1,91 @@
 `timescale 1ns / 1ps
 
-module mac_unit #(
-    parameter [3:0] W0 = 1, W1 = 2, W2 = 3,
-    parameter [3:0] W3 = 4, W4 = 5, W5 = 6,
-    parameter [3:0] W6 = 7, W7 = 8, W8 = 9
-)(
-    input clk,
-    input resetn,
-    input [7:0] a0,a1,a2,a3,a4,a5,a6,a7,a8,
-    output reg [15:0] y
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Shashank Tiwari, Vishal V, Sameeksha
+// Update Date: 22.06.2026
+// Module Name: mac_unit.v
+// Project Name: CNN Summer
+// Description:
+// Single MAC Unit, allows use of signed weights and inputs. Option
+// available in code for direct weights allocaion via parameters or weights value
+// to be taken in from inputs.
+////////////////////////////////////////////////////////////////////////////////////
+
+// `define DIRECT_WEIGHTS
+
+module mac_unit
+`ifdef DIRECT_WEIGHTS
+#(
+	parameter signed [3:0] w0 = -8, w1 = -8, w2 = -8,
+	parameter signed [3:0] w3 = -8, w4 = -8, w5 = -8,
+	parameter signed [3:0] w6 = -8, w7 = -8, w8 = -8
+)
+`endif
+(
+	input clk,
+	input resetn,
+	input [7:0] a0,a1,a2,a3,a4,a5,a6,a7,a8,
+`ifndef DIRECT_WEIGHTS
+	input signed [3:0] w0,w1,w2,w3,w4,w5,w6,w7,w8,
+`endif
+	output reg signed [15:0] y
 );
-    wire [15:0] value;
-    wire [12:0] m0,m1,m2,m3,m4,m5,m6,m7,m8;
 
-    assign m0 = a0*W0;
-    assign m1 = a1*W1;
-    assign m2 = a2*W2;
-    assign m3 = a3*W3;
-    assign m4 = a4*W4;
-    assign m5 = a5*W5;
-    assign m6 = a6*W6;
-    assign m7 = a7*W7;
-    assign m8 = a8*W8;
+//-----------------------//
+// Sign Extending Inputs //
+//-----------------------//
+wire signed [8:0] a0_s = {1'b0,a0};
+wire signed [8:0] a1_s = {1'b0,a1};
+wire signed [8:0] a2_s = {1'b0,a2};
+wire signed [8:0] a3_s = {1'b0,a3};
+wire signed [8:0] a4_s = {1'b0,a4};
+wire signed [8:0] a5_s = {1'b0,a5};
+wire signed [8:0] a6_s = {1'b0,a6};
+wire signed [8:0] a7_s = {1'b0,a7};
+wire signed [8:0] a8_s = {1'b0,a8};
 
-    assign value = m0+m1+m2+m3+m4+m5+m6+m7+m8;
+//---------------------------------------//
+// Intermediate Multiplication Registers //
+//---------------------------------------//
+reg signed [12:0] m0,m1,m2,m3,m4,m5,m6,m7,m8;
 
-    always @(posedge clk) begin
-        if (!resetn)
-            y <= 0;
-        else
-            y <= value;
-    end
+//-----------------------------//
+// Wire for Addition Operation //
+//-----------------------------//
+wire signed [15:0] value;
+
+/*
+ * Driving final Ouput port Sequqntially to avoid
+ * timing issues. Allows using simple testbench to
+ * accurately test the logic.
+ */ 
+always @(posedge clk) 
+begin
+	if(!resetn)
+		y <= 0;
+	else
+		y <= value;
+end
+
+/*
+ * Parallel 9 Multilpication Units.
+ * Inputs a0 to a9 sign extended to prevent multiplication output
+ * to have unsigned value.
+ * $signed used typecast multipliers in RTL
+ */
+always@(*)
+begin
+	m0 = a0_s * $signed(w0);
+	m1 = a1_s * $signed(w1);
+	m2 = a2_s * $signed(w2);
+	m3 = a3_s * $signed(w3);
+	m4 = a4_s * $signed(w4);
+	m5 = a5_s * $signed(w5);
+	m6 = a6_s * $signed(w6);
+	m7 = a7_s * $signed(w7);
+	m8 = a8_s * $signed(w8);
+end
+
+assign value = m0 + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8;
+
 endmodule
