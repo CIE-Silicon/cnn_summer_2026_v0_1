263cdcd9bf6907ec8d8ab9dc3722c9add3fac422
sameeksha <152643698+saameekshaa@users.noreply.github.com>
2026-07-08 10:30:47 +0530

    Update mac_parallel.v

--- mac_unit/mac_parallel.v
+++ mac_unit/mac_parallel.v
@@ -2,10 +2,10 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 // Engineer: Shashank Tiwari, Vishal V, Sameeksha
-// Update Date: 22.06.2026
+// Update Date: 03.07.2026
 // Module Name: mac_parallel.v
 // Project Name: CNN Summer
-// Description:
+// Description: Added 4 new signals
 // Multiple MAC_Units Instantiater.
 ////////////////////////////////////////////////////////////////////////////////////
 
@@ -15,10 +15,14 @@ module mac_parallel
 ) (
 	input clk,
 	input resetn,
-	input [7:0] a0, a1, a2, a3, a4, a5, a6, a7, a8,
+	input [7:0] a0, a1, a2, a3, a4, a5, a6, a7, a8,// from window_generator
+	input wire window_valid, //from window_generator
+	input wire weight_ready, //from weight_fsm 
+	output reg mac_valid, //goes to store_fsm and window generator
+	input wire store_halt, //from store_fsm
     
 	// Standard runtime port vectors for weights
-	input  [(4 * NUM_UNITS) - 1 : 0] w0,
+	input  [(4 * NUM_UNITS) - 1 : 0] w0,//assume
 	input  [(4 * NUM_UNITS) - 1 : 0] w1,
 	input  [(4 * NUM_UNITS) - 1 : 0] w2,
 	input  [(4 * NUM_UNITS) - 1 : 0] w3, 
@@ -27,9 +31,19 @@ module mac_parallel
 	input  [(4 * NUM_UNITS) - 1 : 0] w6, 
 	input  [(4 * NUM_UNITS) - 1 : 0] w7, 
 	input  [(4 * NUM_UNITS) - 1 : 0] w8,
-
 	output [(16 * NUM_UNITS) - 1 : 0] y
+	
 );
+
+
+always@(posedge clk or negedge resetn) begin
+    if (!resetn) begin
+        mac_valid <= 1'b0;
+    end
+    else begin
+        mac_valid <= window_valid && weight_ready && !store_halt && !mac_valid;
+    end
+end      
 
 genvar i;
 generate
